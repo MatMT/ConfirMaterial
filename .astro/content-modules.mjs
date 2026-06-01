03b950b657cdaff9c3fafcc290cd85e5e8e9c8ec
Oscar <oscar@example.com>
2026-05-31 21:54:59 -0600

    feat: implement landing and lesson listing pages with updated Astro and React dependencies

--- .astro/content-modules.mjs
+++
@@ -1,6 +0,0 @@
-
-export default new Map([
-["src/content/lessons/01confirmacion.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2F01confirmacion.mdx&astroContentModuleFlag=true")],
-["src/content/lessons/03pendiente.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2F03pendiente.mdx&astroContentModuleFlag=true")],
-["src/content/lessons/02tecnología.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Flessons%2F02tecnolog%C3%ADa.mdx&astroContentModuleFlag=true")]]);
-		
